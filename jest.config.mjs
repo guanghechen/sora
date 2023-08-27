82c3198764dff25bf533d682513540a90f7cb019
guanghechen <me.guanghechen@gmail.com>
2023-08-27 19:05:59 +0800

    :wrench:  chore: fix test coverage config

--- jest.config.mjs
+++ jest.config.mjs
@@ -7,6 +7,7 @@ const require = createRequire(import.meta.url)
 
 export default async function () {
   const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
+  const packageDir = path.relative(__dirname, path.resolve()) + '/'
   const baseConfig = await tsMonorepoConfig(__dirname, {
     useESM: true,
     tsconfigFilepath: path.join(__dirname, 'tsconfig.test.esm.json'),
@@ -24,6 +25,14 @@ export default async function () {
         statements: 100,
       },
     },
+    coverageThreshold: Object.fromEntries(
+      [
+        ['global', { branches: 100, functions: 100, lines: 100, statements: 100 }],
+        ['packages/pipeline/src/pipeline.ts', { functions: 90 }],
+      ]
+        .filter(([p]) => !p.startsWith('packages/') || p.startsWith(packageDir))
+        .map(([p, val]) => (p.startsWith(packageDir) ? [path.join(__dirname, p), val] : [p, val])),
+    ),
     extensionsToTreatAsEsm: ['.ts', '.mts'],
     prettierPath: require.resolve('prettier-2'),
   }
