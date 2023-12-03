e216311ee58f5d7b442464b47a30e7a65abc671d
guanghechen <me.guanghechen@gmail.com>
2023-12-03 12:40:27 +0800

    :art:  improve(vfs.types): add createReadStream and createWriteStream to IVirtualFileSystem

--- packages/vfs.types/src/filesystem.ts
+++ packages/vfs.types/src/filesystem.ts
@@ -1,4 +1,5 @@
 import type { IDisposable } from '@guanghechen/disposable.types'
+import type { ReadStream, WriteStream } from 'node:fs'
 import type { VfsErrorCode } from './constant'
 import type { IVfsFileStat } from './stat'
 
@@ -12,34 +13,52 @@ export interface IVfsFileWatchOptions {
 
 export interface IVirtualFileSystem extends IDisposable {
   /**
-   *
-   * @param virtualPath
-   * @param options
+   * Copy files or folders, speedup the copy operation.
+   * @param sourceVirtualPath
+   * @param targetVirtualPath
+   * @param overwrite
    */
-  watch(
-    virtualPath: string,
-    options: IVfsFileWatchOptions,
-  ):
-    | VfsErrorCode.SOURCE_NOT_FOUND // When `virtualPath` doesn't exist.
-    | IDisposable // Dispose the watcher. // When the operation is executed successfully.
-
-  /**
-   * Retrieve metadata about a file.
-   * @param virtualPath
-   */
-  stat(virtualPath: string): Promise<
-    | VfsErrorCode.SOURCE_NOT_FOUND // When `virtualPath` doesn't exist.
-    | IVfsFileStat // File metadata .// When the operation is executed successfully.
+  copy(
+    sourceVirtualPath: string,
+    targetVirtualPath: string,
+    overwrite: boolean,
+  ): Promise<
+    | VfsErrorCode.SOURCE_NOT_FOUND // When `sourceVirtualPath` doesn't exist.
+    | VfsErrorCode.PARENT_TARGET_NOT_FOUND // When parent of `targetVirtualPath` doesn't exist.
+    | VfsErrorCode.PARENT_TARGET_NOT_DIRECTORY // When parent of `targetVirtualPath` is not a directory.
+    | VfsErrorCode.TARGET_EXIST // When `targetVirtualPath` exists and when the `overwrite` is not `true`.
+    | VfsErrorCode.TARGET_IS_DIRECTORY // When `sourceVirtualPath` is not a directory but `targetVirtualPath` is a directory.
+    | VfsErrorCode.TARGET_NOT_DIRECTORY // When `sourceVirtualPath` is a directory but `targetVirtualPath` is not a directory.
+    | VfsErrorCode.TARGET_NO_PERMISSION // When permission aren't sufficient.
+    | void // When the operation is executed successfully.
   >
 
   /**
-   * Retrieve all entries of a {@link FileType.Directory directory}.
+   * Create a readable stream to read the entire contents of a file.
    * @param virtualPath
+   * @param options
    */
-  readdir(virtualPath: string): Promise<
-    | VfsErrorCode.SOURCE_NOT_FOUND // When the  `virtualPath` doesn't exist.
-    | VfsErrorCode.SOURCE_NOT_DIRECTORY // When the `virtualPath` is not a directory.
-    | string[] // filenames under the dir. // When the operation is executed successfully.
+  createReadStream(
+    virtualPath: string,
+    options?: BufferEncoding,
+  ): Promise<
+    | VfsErrorCode.SOURCE_NOT_FOUND // When `virtualPath` doesn't exist.
+    | VfsErrorCode.SOURCE_IS_DIRECTORY // When the `virtualPath` is a directory.
+    | ReadStream // When the operation is executed successfully.
+  >
+
+  /**
+   * Create a writable stream to write data to a file, replacing its entire contents.
+   * @param virtualPath
+   * @param options
+   */
+  createWriteStream(
+    virtualPath: string,
+    options?: BufferEncoding,
+  ): Promise<
+    | VfsErrorCode.PARENT_SOURCE_NOT_FOUND // When the parent of `virtualPath` doesn't exist.
+    | VfsErrorCode.PARENT_SOURCE_NOT_DIRECTORY // When the parent of `virtualPath` is not a directory.
+    | WriteStream // When the operation is executed successfully.
   >
 
   /**
@@ -65,24 +84,13 @@ export interface IVirtualFileSystem extends IDisposable {
   >
 
   /**
-   * Write data to a file, replacing its entire contents.
+   * Retrieve all entries of a {@link FileType.Directory directory}.
    * @param virtualPath
-   * @param content
-   * @param create
-   * @param overwrite
    */
-  write(
-    virtualPath: string,
-    content: Uint8Array,
-    create: boolean,
-    overwrite: boolean,
-  ): Promise<
-    | VfsErrorCode.SOURCE_NOT_FOUND // When the `virtualPath` doesn't exist and `create` is not `true`.
-    | VfsErrorCode.PARENT_SOURCE_NOT_FOUND // When the parent of `virtualPath` doesn't exist and `create` is `true`.
-    | VfsErrorCode.PARENT_SOURCE_NOT_DIRECTORY // When the parent of `virtualPath` is not a directory
-    | VfsErrorCode.SOURCE_EXIST // When the `virtualPath` already exists, `create` is set but `overwrite` is not `true`.
-    | VfsErrorCode.SOURCE_NO_PERMISSION // When permissions aren't sufficient.
-    | void // When the operation is executed successfully.
+  readdir(virtualPath: string): Promise<
+    | VfsErrorCode.SOURCE_NOT_FOUND // When the  `virtualPath` doesn't exist.
+    | VfsErrorCode.SOURCE_NOT_DIRECTORY // When the `virtualPath` is not a directory.
+    | string[] // filenames under the dir. // When the operation is executed successfully.
   >
 
   /**
@@ -101,43 +109,64 @@ export interface IVirtualFileSystem extends IDisposable {
 
   /**
    * Rename the file.
-   * @param source
-   * @param target
+   * @param sourceVirtualPath
+   * @param targetVirtualPath
    * @param overwrite
    */
   rename(
-    source: string,
-    target: string,
+    sourceVirtualPath: string,
+    targetVirtualPath: string,
     overwrite: boolean,
   ): Promise<
-    | VfsErrorCode.SOURCE_NOT_FOUND // When `source` doesn't exist.
-    | VfsErrorCode.PARENT_TARGET_NOT_FOUND // When parent of `target` doesn't exist.
-    | VfsErrorCode.PARENT_TARGET_NOT_DIRECTORY // When parent of `target` is not a directory.
-    | VfsErrorCode.TARGET_EXIST // When `target` exists and when the `overwrite` is not `true`.
-    | VfsErrorCode.TARGET_IS_DIRECTORY // When `source` is not a directory but `target` is a directory.
-    | VfsErrorCode.TARGET_NOT_DIRECTORY // When `source` is a directory but `target` is not a directory.
+    | VfsErrorCode.SOURCE_NOT_FOUND // When `sourceVirtualPath` doesn't exist.
+    | VfsErrorCode.PARENT_TARGET_NOT_FOUND // When parent of `targetVirtualPath` doesn't exist.
+    | VfsErrorCode.PARENT_TARGET_NOT_DIRECTORY // When parent of `targetVirtualPath` is not a directory.
+    | VfsErrorCode.TARGET_EXIST // When `targetVirtualPath` exists and when the `overwrite` is not `true`.
+    | VfsErrorCode.TARGET_IS_DIRECTORY // When `sourceVirtualPath` is not a directory but `targetVirtualPath` is a directory.
+    | VfsErrorCode.TARGET_NOT_DIRECTORY // When `sourceVirtualPath` is a directory but `targetVirtualPath` is not a directory.
     | VfsErrorCode.TARGET_NO_PERMISSION // When permissions aren't sufficient.
     | void // When the operation is executed successfully.
   >
 
   /**
-   * Copy files or folders, speedup the copy operation.
-   * @param source
-   * @param target
+   * Retrieve metadata about a file.
+   * @param virtualPath
+   */
+  stat(virtualPath: string): Promise<
+    | VfsErrorCode.SOURCE_NOT_FOUND // When `virtualPath` doesn't exist.
+    | IVfsFileStat // File metadata .// When the operation is executed successfully.
+  >
+
+  /**
+   *
+   * @param virtualPath
+   * @param options
+   */
+  watch(
+    virtualPath: string,
+    options: IVfsFileWatchOptions,
+  ):
+    | VfsErrorCode.SOURCE_NOT_FOUND // When `virtualPath` doesn't exist.
+    | IDisposable // Dispose the watcher. // When the operation is executed successfully.
+
+  /**
+   * Write data to a file, replacing its entire contents.
+   * @param virtualPath
+   * @param content
+   * @param create
    * @param overwrite
    */
-  copy(
-    source: string,
-    target: string,
+  write(
+    virtualPath: string,
+    content: Uint8Array,
+    create: boolean,
     overwrite: boolean,
   ): Promise<
-    | VfsErrorCode.SOURCE_NOT_FOUND // When `source` doesn't exist.
-    | VfsErrorCode.PARENT_TARGET_NOT_FOUND // When parent of `target` doesn't exist.
-    | VfsErrorCode.PARENT_TARGET_NOT_DIRECTORY // When parent of `target` is not a directory.
-    | VfsErrorCode.TARGET_EXIST // When `target` exists and when the `overwrite` is not `true`.
-    | VfsErrorCode.TARGET_IS_DIRECTORY // When `source` is not a directory but `target` is a directory.
-    | VfsErrorCode.TARGET_NOT_DIRECTORY // When `source` is a directory but `target` is not a directory.
-    | VfsErrorCode.TARGET_NO_PERMISSION // When permission aren't sufficient.
+    | VfsErrorCode.SOURCE_NOT_FOUND // When the `virtualPath` doesn't exist and `create` is not `true`.
+    | VfsErrorCode.PARENT_SOURCE_NOT_FOUND // When the parent of `virtualPath` doesn't exist and `create` is `true`.
+    | VfsErrorCode.PARENT_SOURCE_NOT_DIRECTORY // When the parent of `virtualPath` is not a directory
+    | VfsErrorCode.SOURCE_EXIST // When the `virtualPath` already exists, `create` is set but `overwrite` is not `true`.
+    | VfsErrorCode.SOURCE_NO_PERMISSION // When permissions aren't sufficient.
     | void // When the operation is executed successfully.
   >
 }
