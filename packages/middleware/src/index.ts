ba84d0ca1d43a4778ab9551f7c8876d3b1c7fb94
guanghechen <me.guanghechen@gmail.com>
2024-03-09 17:40:11 +0800

    :art:  refactor middleware & remove middleware.types

--- packages/middleware/src/index.ts
+++ packages/middleware/src/index.ts
@@ -1,5 +1,2 @@
 export * from './async'
 export * from './sync'
-
-// Re-export types and constants
-export * from '@guanghechen/middleware.types'
--- packages/middleware/src/sync.ts
+++ packages/middleware/src/sync.ts
@@ -1,4 +1,29 @@
-import type { IMiddleware, IMiddlewareNext, IMiddlewares } from '@guanghechen/middleware.types'
+export interface IMiddlewareNext<Output> {
+  (embryo: Readonly<Output | null>): Output | null
+}
+
+export interface IMiddleware<Input, Output, Api> {
+  (
+    input: Readonly<Input>,
+    embryo: Readonly<Output> | null,
+    api: Readonly<Api>,
+    next: IMiddlewareNext<Output>,
+  ): Output | null
+}
+
+export interface IMiddlewares<Input, Output, Api> {
+  /**
+   * Use a middleware.
+   * @param middleware
+   */
+  use(middleware: IMiddleware<Input, Output, Api>): void
+  /**
+   * Run with the middlewares.
+   * @param input
+   * @param api
+   */
+  reducer(input: Readonly<Input>, api: Readonly<Api>): IMiddlewareNext<Output>
+}
 
 export class Middlewares<Input, Output, Api> implements IMiddlewares<Input, Output, Api> {
   protected readonly _middlewares: Array<IMiddleware<Input, Output, Api>>
