391f42f68f1a0fe8893f64025b8cfec2728d368d
guanghechen <me.guanghechen@gmail.com>
2024-03-10 19:35:34 +0800

    :art:  improve(viewmodel): re-export subscriber and disposable

--- packages/viewmodel/src/index.ts
+++ packages/viewmodel/src/index.ts
@@ -11,5 +11,11 @@ export * from './types/observable'
 export * from './types/state'
 export * from './types/viewmodel'
 
+// Re-export disposable.
+export * from '@guanghechen/disposable'
+
 // Re-export observable.
 export * from '@guanghechen/observable'
+
+// Re-export subscriber.
+export * from '@guanghechen/subscriber'
