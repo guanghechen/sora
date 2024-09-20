376a7cf82cbd31a43d3aca3e5f51e521549c2af4
guanghechen <me.guanghechen@gmail.com>
2024-09-20 12:03:37 +0800

    :wrench:  chore: fix nx config

--- rollup.config.lib.mjs
+++ rollup.config.lib.mjs
@@ -18,14 +18,17 @@ const uselessImportRegex = new RegExp(
 )
 
 export default async function rollupConfig() {
+  const sourcemapFromCLI = process.argv
+    .filter(arg => arg.startsWith('--sourcemap='))
+    .map(arg => arg.split('=')[1])
+  const sourcemap = sourcemapFromCLI.length > 0 ? sourcemapFromCLI[0] === 'true' : false
+
   const { default: manifest } = await import(path.resolve('package.json'), {
     assert: { type: 'json' },
   })
   const config = await createRollupConfig({
     manifest,
-    env: {
-      sourcemap: false,
-    },
+    env: { sourcemap },
     presetConfigBuilders: [
       tsPresetConfigBuilder({
         typescriptOptions: {
