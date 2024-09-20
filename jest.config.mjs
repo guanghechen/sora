03c44afc3f8902278963cdb088c7481f13c3811d
guanghechen <me.guanghechen@gmail.com>
2024-09-20 11:25:38 +0800

    :sparkles:  feat: add @guanghechen/string (migrated from node-scaffolds)

--- jest.config.mjs
+++ jest.config.mjs
@@ -53,6 +53,10 @@ const coverageMap = {
   '@guanghechen/scheduler': {
     global: { branches: 86, functions: 97, lines: 95, statements: 95 },
   },
+  '@guanghechen/string': {
+    'src/vender/change-case.ts': { branches: 53, functions: 82 },
+    'src/vender/title-case.ts': { branches: 50 },
+  },
   '@guanghechen/subscriber': {
     global: { functions: 93 },
   },
