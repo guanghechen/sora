23b8a950cd3ea8a4487c5593d307e7321bddffde
guanghechen <me.guanghechen@gmail.com>
2026-02-03 13:57:04 +0800

    :arrow_up: chore: upgrade dependencies
    - @types/node: 25.1.0 → 25.2.0
    - rollup: 4.57.0 → 4.57.1

--- packages/viewmodel/src/types/observable.ts
+++ packages/viewmodel/src/types/observable.ts
@@ -6,8 +6,11 @@ export interface IObservableCollectionOptions<V> {
   valueEquals?: IEquals<V | undefined>
 }
 
-export interface IObservableCollection<K, V, C extends IImmutableCollection<K, V>>
-  extends IObservable<C> {
+export interface IObservableCollection<
+  K,
+  V,
+  C extends IImmutableCollection<K, V>,
+> extends IObservable<C> {
   has(key: K): boolean
   get(key: K): V | undefined
   keys(): Iterable<K>
@@ -17,16 +20,22 @@ export interface IObservableCollection<K, V, C extends IImmutableCollection<K, V
   subscribeKey(key: K, subscriber: ISubscriber<V | undefined>): IUnsubscribable
 }
 
-export interface IObservableMap<K, V, C extends IImmutableMap<K, V>>
-  extends IObservableCollection<K, V, C> {
+export interface IObservableMap<K, V, C extends IImmutableMap<K, V>> extends IObservableCollection<
+  K,
+  V,
+  C
+> {
   set(key: K, value: V, options?: IObservableNextOptions): void
   delete(key: K, options?: IObservableNextOptions): void
   deleteAll(keys: Iterable<K>, options?: IObservableNextOptions): void
   merge(entries: Iterable<[K, V]>, options?: IObservableNextOptions): void
 }
 
-export interface IObservableSet<V, C extends IImmutableSet<V>>
-  extends IObservableCollection<V, V, C> {
+export interface IObservableSet<V, C extends IImmutableSet<V>> extends IObservableCollection<
+  V,
+  V,
+  C
+> {
   add(value: V, options?: IObservableNextOptions): void
   addAll(values: Iterable<V>, options?: IObservableNextOptions): void
   delete(value: V, options?: IObservableNextOptions): void
